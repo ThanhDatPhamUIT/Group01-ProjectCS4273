173d572660affaf464474a84002e720c4b100b05
Dat Pham Thanh <75928178+kudat-uit@users.noreply.github.com>
2022-12-14 09:33:41 +0700

    Delete Code directory

--- Code/Test/test_manual.py
+++
@@ -1,6 +0,0 @@
-from sample import calculate_sum, find_divisor, check_perfect_number
-
-# Test manual
-test_data = 0
-actual_result = check_perfect_number(test_data)
-print("Actual result: ", actual_result)
